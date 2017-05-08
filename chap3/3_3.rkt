90269e4926b6a829f7d80b18af3dc46db8792c22
yishibashi <yishibashi101@gmail.com>
2017-05-08 22:56:38 +0900

    ~ ex 3.14 +α

---
+++ chap3/3_3.rkt
@@ -0,0 +1,28 @@
+#lang planet neil/sicp
+
+(define (append! x y)
+    (set-cdr! (last-pair x) y)
+    x)
+
+
+(define (last-pair x)
+    (if (null? (cdr x))
+    x
+    (last-pair (cdr x))))
+
+(define (make-cycle x)
+    (set-cdr! (last-pair x) x)
+    x)
+
+(define (mystery x)
+    (define (loop x y)
+        (if (null? x)
+            y
+            (let ((temp (cdr x)))
+                (set-cdr! x y)
+                (loop temp x))))
+     (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+(define w (mystery v))
+w
